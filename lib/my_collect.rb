7bff776941361549031deecbb3ce4d51b2f2cbe9
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-21 00:47:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,28 +10,3 @@ def my_collect(array)
   new_array
 end
 
-
-#   require 'pry'
-# # Method Collect invokes the argument block for each element and returns a new array with updated elements.
-
-
-# def my_collect(array)
-#   array = []
- 
-#   name_list = ["first Last0", "first last1", "first last2"]
-#   language_list = ["lang0", "lan1", "lang2", "lang3"]
- 
-#   counter = 0
-
-#   while counter < array.length do 
-#     array_of_names << (array[counter])
-#     yield(array[counter]) 
-#       puts array.upcase
-#     counter += 1
-#   end
-
-#   return my_collect(language_list)
-
-# end
-
-# my_collect(name_list) |counter| counter.split(" ").first
